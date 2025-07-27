aaefe95b22da3a6c6edb9a00078f673f9904e8d2
M4th3uX <matheus.ulhoa13@gmail.com>
2025-07-27 00:51:41 -0300

    Criação Workflow de Deploy

--- m4th3ux-site/docusaurus.config.js
+++ m4th3ux-site/docusaurus.config.js
@@ -144,7 +144,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} m4th3ux-techlog, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
